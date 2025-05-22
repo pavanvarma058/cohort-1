cea181a552a01846a86c54caf147315385da0131
Pavan Varma <pavanvarma058@gmail.com>
2025-05-22 15:13:50 +0530

    Completed the course selling app by building both admin and user routes

--- week-3/assignments/02-course-app-easy/index.js
+++ week-3/assignments/02-course-app-easy/index.js
@@ -51,37 +51,91 @@ app.post("/admin/login", adminAuthentication, (req, res) => {
   res.json({ message: "Logged in successfully" });
 });
 
-app.post("/admin/courses", (req, res) => {
+app.post("/admin/courses", adminAuthentication, (req, res) => {
   // logic to create a course
+  const course = req.body;
+  if (!course.title) {
+    return res.json(403).send({ message: "Please send me the title" });
+  }
+  course.id = Date.now(); // use timestamp as course ID
+  COURSES.push(course);
+  res.json({ message: "Course created successfully", courseId: course.id });
 });
 
-app.put("/admin/courses/:courseId", (req, res) => {
+app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
   // logic to edit a course
+  const courseId = parseInt(req.params.courseId);
+  const course = COURSES.find((c) => c.id === courseId);
+  if (course) {
+    Object.assign(course, req.body);
+    res.json({ message: "Course update successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
 });
 
-app.get("/admin/courses", (req, res) => {
+app.get("/admin/courses", adminAuthentication, (req, res) => {
   // logic to get all courses
+  res.json({ courses: COURSES });
 });
 
 // User routes
 app.post("/users/signup", (req, res) => {
   // logic to sign up user
+  const user = { ...req.body, purchasedCourses: [] };
+  // const user1 = {
+  //   username: req.body.username,
+  //   password: req.body.password,
+  //   purchasedCourses: [],
+  // };
+  USERS.push(user);
+  res.json({ message: "User created successfully" });
 });
 
-app.post("/users/login", (req, res) => {
+app.post("/users/login", userAuthentication, (req, res) => {
   // logic to log in user
+  res.json({ message: "Logged in successfully" });
 });
 
-app.get("/users/courses", (req, res) => {
-  // logic to list all courses
+app.get("/users/courses", userAuthentication, (req, res) => {
+  // logic to list all
+  // let filteredCourses = [];
+  // for (let i = 0; i < COURSES.length; i++) {
+  //   if (COURSES[i].published) {
+  //     filteredCourses.push(COURSES[i]);
+  //   }
+  // }
+  // filteredCourses is same as COURSES.filter((c) => c.published)
+  res.json({ courses: COURSES.filter((c) => c.published) });
 });
 
-app.post("/users/courses/:courseId", (req, res) => {
+app.post("/users/courses/:courseId", userAuthentication, (req, res) => {
   // logic to purchase a course
+  const courseId = parseInt(req.params.courseId);
+  const course = COURSES.find((c) => c.id === courseId && c.published);
+  if (course) {
+    req.user.purchasedCourses.push(courseId);
+    res.json({ message: "Course purchased successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found or not available" });
+  }
 });
 
-app.get("/users/purchasedCourses", (req, res) => {
+app.get("/users/purchasedCourses", userAuthentication, (req, res) => {
   // logic to view purchased courses
+  // We need to extract the complete course object from COURSES
+  // which have ids similar to which are present in req.user.purchasedCourses
+  var purchasedCourseIds = req.user.purchasedCourses;
+  // var purchasedCourses = [];
+  // for (let i = 0; i < COURSES.length; i++) {
+  //   if (purchasedCourseIds.indexOf(COURSES[i].id) !== -1) {
+  //     purchasedCourses.push(COURSES[i]);
+  //   }
+  // }
+  const purchasedCourses = COURSES.filter((c) =>
+    req.user.purchasedCourses.includes(c.id)
+  );
+  res.json({ purchasedCourses });
 });
 
 app.listen(3000, () => {
